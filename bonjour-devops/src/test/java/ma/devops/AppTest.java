4b36fbe11317eb5761f73bd4e167cc4ec70d28fb
ImadAkabli <imadakabliofficial@gmail.com>
2026-01-09 16:22:31 +0100

    Add Maven app bonjour-devops and gitignore

---
+++ bonjour-devops/src/test/java/ma/devops/AppTest.java
@@ -0,0 +1,11 @@
+package ma.devops;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    void testSimple() {
+        assertTrue(true);
+    }
+}
